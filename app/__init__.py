ece0292c1e4cd2229847c55924b1db689228f07e
Hampus Rude <hrude@kth.se>
2018-11-27 21:10:44 +0100

    Tog bort statistik-sida for now

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import requests
 
-# TODO: Kolla videon om packeging igen
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c67a60abf8486349a4d5ad912aaf0d1f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -22,4 +22,23 @@ app.config['MAIL_USERNAME'] = 'ononabtest@gmail.com'
 app.config['MAIL_PASSWORD'] = 'Qwe3945o!'
 mail = Mail(app)
 
+'''
+# Configuring webhook
+access_token = "eu8UCOb1ARZae00whmCKNwA0d-kYUfA45emNHAvblRVbZUD7fS8NgITq.Bo34b88zKEz97YERLKNj.T0Y3HiRFhqr5jXnhjIi3J1POlFNJ7ZR03B8JmkAv-jkGvUfjf9"
+s = requests.session()
+s.headers.update({
+  "Authorization": "Bearer %s" % access_token,
+  "Content-Type": "application/json"
+})
+
+payload = {
+  "name": "My Webhook",
+  "event_type": "response_completed",
+  "object_type": "survey",
+  "object_ids": ["160620263"],
+  "subscription_url": "http://727ee712.ngrok.io/webhook"
+}
+url = "https://api.surveymonkey.com/v3/webhook"
+s.post(url, json=payload)
+'''
 from app import routes
