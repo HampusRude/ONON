3944ff2d382d930089687231e5fd00f84857aed9
Johan Dahl <johad@kth.se>
2018-12-13 18:47:27 +0100

    method to add single response complete

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c67a60abf8486349a4d5ad912aaf0d1f'
 
-
-#params = urllib.parse.quote_plus("DRIVER={ODBC Driver 13 for SQL Server};SERVER=onondb.database.windows.net;DATABASE=onondb;UID=rudahl@onondb;PWD=t2jguGe83oxwYgeV")
-#db_uri = "mssql+pyodbc:///?odbc_connect=%s" % params
-# db_uri = 'mssql+pyodbc://rudahl:t2jguGe83oxwYgeV@ononapp.database.windows.net:1433/onondb?driver=ODBC+Driver+13+for+SQL+Server'
 db_uri = 'sqlite:///site.db'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 
 db = SQLAlchemy(app)
