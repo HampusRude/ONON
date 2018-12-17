10dc5f3df4ee65b40f05931e3124d8795069a0b5
Johan Dahl <johad@kth.se>
2018-12-17 15:33:40 +0100

    changed to environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -5,18 +5,6 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
-"""
-os.environ['DB_USER'] = 'HampusRude@hampustest'
-os.environ['DB_PASSWORD'] = 'Password!'
-os.environ['DB_URL'] = 'hampustest.postgres.database.azure.com'
-os.environ['DB_DB'] = 'postgres'
-os.environ['SECRET_KEY'] = 'c67a60abf8486349a4d5ad912aaf0d1f'
-os.environ['SM_ACCESS_TOKEN'] = 'eu8UCOb1ARZae00whmCKNwA0d-kYUfA45emNHAvblRVbZUD7fS8NgITq.Bo34b88zKEz97YERLKNj.T0Y3HiRFhqr5jXnhjIi3J1POlFNJ7ZR03B8JmkAv-jkGvUfjf9'
-os.environ['MAIL_SERVER'] = 'smtp.googlemail.com'
-os.environ['MAIL_PORT'] = '587'
-os.environ['MAIL_USERNAME'] = 'noreplybehovsanalystest@gmail.com'
-os.environ['MAIL_PASSWORD'] = 'SecretPassword!'
-"""
 
 db_uri = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=os.environ['DB_USER'],
                                                                pw=os.environ['DB_PASSWORD'],
